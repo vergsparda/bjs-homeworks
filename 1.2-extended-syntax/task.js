cd9fb0a9f7e4888932dbb35467ecfef1158437f1
Alex Smith <alex.vesson.smith@gmail.com>
2020-08-17 22:14:23 +0300

    ешена первая задача

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,17 @@
+'use strict'
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+  let discriminant = Math.pow(b, 2) - (4 * a * c);
+  let x = [];
+  if (discriminant > 0 ) {
+    x[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
+    x[1] = (-b - Math.sqrt(discriminant)) / (2 * a)
+    } else if (discriminant === 0) {
+      x[0] = -b / 2 * a;
+    } else if (discriminant < 0) {
+      x = [];
+    }
+  return x;
 }
 
 function getAverageMark(marks){
