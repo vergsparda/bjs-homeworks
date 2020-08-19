b5de532989c7288504dc105b5b57765925279711
Alex Smith <alex.vesson.smith@gmail.com>
2020-08-19 20:32:57 +0300

    All issues fixed.

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -6,53 +6,39 @@ function getResult(a,b,c){
   if (discriminant > 0 ) {
     x[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
     x[1] = (-b - Math.sqrt(discriminant)) / (2 * a)
-    } else if (discriminant === 0) {
-      x[0] = -b / 2 * a;
-    } else if (discriminant < 0) {
-      x = [];
-    }
+  } else if (discriminant === 0) {
+    x[0] = -b / 2 * a;
+  }
+
   return x;
 }
 
 function getAverageMark(marks){
   let sum = 0;
-  let averageMark;
   console.log(marks.length)
 
-  function getSumMarks() {
-    for (let i = 0; i < marks.length; i++) {
-      sum += marks[i];
-    }
-  }
-
-  if (marks.length === NaN) {
-    averageMark = 0;
+  if (marks.length === 0) {
+    return 0;
   } else if (marks.length > 5) {
     console.log('оценок больше пяти')
-    marks.splice(5, 999);
+    marks.splice(5);
     console.log(marks);
-    getSumMarks(marks.length);
-  } else if (marks.length <= 5) {
-    console.log(marks);
-    getSumMarks(marks.length);
   }
-    averageMark = sum / marks.length;
 
-  return averageMark;
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+  }
+
+  return sum / marks.length;
 }
 
 function askDrink(name,dateOfBirthday){
-  let userName = name;
-  let userBirthYear = dateOfBirthday.getFullYear();
-  let now = new Date();
-  let currentYear = now.getFullYear();
-  let userAge = currentYear - userBirthYear;
+  const userAge = new Date().getFullYear() - dateOfBirthday.getFullYear();
   let drink;
   if (userAge >= 18) {
-    drink = `Не желаете ли олд-фэшн, ${userName}?`
-  } else if(userAge < 18) {
-    drink= `Сожалею, ${userName}, но я не могу вам продать алкоголь. 
-    Зато могу предложить вам замечательный клюквенный компот!`
+    drink = `Не желаете ли олд-фэшн, ${name}?`;
+  } else {
+    drink = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
   }
   return drink;
 }
