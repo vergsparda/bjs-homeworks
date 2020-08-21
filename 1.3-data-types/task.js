5aad24ba830e8994babc0ac66e62267eaa145226
Alex Smith <alex.vesson.smith@gmail.com>
2020-08-21 21:13:08 +0300

    Решена задача №1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,21 @@
+'use strict'
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  let body = +amount - +contribution;
+  let monthTotal = ((date.getFullYear() - new Date().getFullYear()) * 12 + date.getMonth()) - new Date().getMonth();
+  let monthPercent = (+percent / 100) / 12;
+  let monthlyTax = body * (monthPercent + monthPercent/(((1 + monthPercent)**monthTotal) - 1));
+  let totalAmount = (monthTotal * monthlyTax).toFixed(2);
+
+  if (Object.is(percent, NaN) || percent === null || percent === undefined){
+    return "Параметр 'Процентная ставка' содержит неправильное значениe " + percent;
+  } else if (Object.is(contribution, NaN) || contribution === null || contribution === undefined){
+    return "Параметр 'Сумма первоночального взноса' содержит неправильное значениe " + contribution;
+  } else if (Object.is(amount, NaN) || amount === null || amount === undefined){
+    return "Параметр 'Сумма кредита' содержит неправильное значениe " + amount;
+  }
+
+    return parseFloat(totalAmount);
 }
 
 function getGreeting(name) {
