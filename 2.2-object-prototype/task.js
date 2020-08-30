3deacbd849b9f97332ab71ba76d93317ac4b8955
Alex Smith <alex.vesson.smith@gmail.com>
2020-08-30 15:29:30 +0300

    Решены задачи №1 и 2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,33 @@
-//String.prototype.isPalindrome - для задачи №1
+`use strict`
+// Задача №1
+String.prototype.isPalindrome = function () {
+  let message = this.toUpperCase()
+  let result;
+  for (let i = 0; i < message.length; i++) {
+    if (message[i] === message[message.length - 1 - i]) {
+      result = true
+    } else {
+      result = false
+    }
+    return result;
+  }
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let sum = 0
+  let averageMark
+  if (marks != 0) {
+    for (let i = 0; i < marks.length; i++) {
+      sum += marks[i]
+      averageMark = Math.round(sum / marks.length)
+    }
+  } else {
+    averageMark = 0
+  }
+  return averageMark
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  // код для задачи №3 писать здесь
+  // return verdict
 }
