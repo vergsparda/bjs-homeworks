ea76d2be9090fb2eacab3bd0d8380c21893f2b24
Alex Smith <alex.vesson.smith@gmail.com>
2020-09-13 22:47:41 +0300

    все задачи решены!

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -5,19 +5,19 @@
 console.clear();
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 
-function getNames(){
+function getNames() {
   return weapons.map(weapon => weapon.name);
 }
 
-function getCountReliableWeapons(a){
+function getCountReliableWeapons(a) {
   return weapons.filter(weapon => weapon.durability > a).length;
 }
 
-function hasReliableWeapons(a){
+function hasReliableWeapons(a) {
   return weapons.some(weapon => weapon.durability > a);
 }
 
-function getReliableWeaponsNames(a){
+function getReliableWeaponsNames(a) {
   let names = [];
   weapons.forEach(weapon => {
     if (weapon.durability > a) {
@@ -35,8 +35,8 @@ function getTotalDamage() {
 
 function getValuestCountToSumValues(arr, number) {
   let result = arr.length;
-  arr.reduce(function(acc, item, index){
-    if(acc >= number) {
+  arr.reduce(function (acc, item, index) {
+    if (acc >= number) {
       result = index;
       return;
     } else return acc += item;
@@ -44,20 +44,19 @@ function getValuestCountToSumValues(arr, number) {
   return result;
 }
 
-console.log(getValuestCountToSumValues([1,2,3,5,2,7,3,5,2], 10));
-console.log(getValuestCountToSumValues([1,2,3,5,2,7,3,5,2], 50));
+console.log(getValuestCountToSumValues([1, 2, 3, 5, 2, 7, 3, 5, 2], 10));
+console.log(getValuestCountToSumValues([1, 2, 3, 5, 2, 7, 3, 5, 2], 50));
 
 // Задача №2
 
-function sleep(milliseconds)
-{
+function sleep(milliseconds) {
   let e = new Date().getTime() + milliseconds;
   while (new Date().getTime() <= e) {}
 }
 
 function sum(...args) {
   // Замедление на половину секунды.
-  sleep(300); // Можно использовать другое значение замедления.
+  sleep(200); // Можно использовать другое значение замедления.
   return args.reduce((sum, arg) => {
     return sum += +arg;
   }, 0);
@@ -68,8 +67,25 @@ function compareArrays(arr1, arr2) {
   return arr1.every((item, index) => item === arr2[index]);
 }
 
+function memorize(fn, limit) {
+  let memory = [];
+  return function (...args) {
+    const oldResult = memory.find(item => compareArrays(args, item.args));
+    if (oldResult !== undefined) return oldResult.result;
+    const newResult = fn(...args);
+    if (memory.length === limit) {
+      memory.shift();
+      memory.push({
+        args: args,
+        result: newResult
+      });
+    }
+    return newResult;
+  }
+}
+
 const mSum = memorize(sum, 5); // 5 результатов может хранится в памяти
 sum(3, 4); // 7
-  // разница только в том, что mSum запоминает результат (7)
-  // и повторно не делает вычисления
+// разница только в том, что mSum запоминает результат (7)
+// и повторно не делает вычисления
 mSum(3, 4); // 7
